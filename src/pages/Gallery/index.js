60da02a63c3aa0bce3e462d304b313941d567422
Raj <mitizdev@gmail.com>
2025-05-07 10:44:35 +0530

    loading added

--- src/pages/Gallery/index.js
+++ src/pages/Gallery/index.js
@@ -26,7 +26,7 @@ function Gallery() {
                     : data.filter((item) => item.category === selectedCategory);
             setFilteredData(filtered);
             setLoading(false);
-        }, 1000); // simulate loading delay
+        }, 2000); // simulate loading delay
 
         return () => clearTimeout(timer);
     }, [selectedCategory]);
